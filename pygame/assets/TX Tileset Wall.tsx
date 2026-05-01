4bc94cd33f8ce2bf31dfe669898eeab14601bb47
jrayverbautista <jeffrayverbautista@gmail.com>
2026-05-01 14:47:46 +0800

    Add files via upload

---
+++ pygame/assets/TX Tileset Wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="TX Tileset Wall" tilewidth="16" tileheight="16" tilecount="418" columns="22">
+ <image source="TX Tileset Wall.png" trans="ffffff" width="361" height="313"/>
+</tileset>
